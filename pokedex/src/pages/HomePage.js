b08b90d4f51ffe82ae56dcb748870b3a21636418
Fayra Miranda <miranda.fayra@gmail.com>
2021-04-19 19:27:42 -0300

    Estruturar Header

--- pokedex/src/pages/HomePage.js
+++ pokedex/src/pages/HomePage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Header from '../components/Header'
 
 const HomePage = () => {
-
-
   return (
     <>
+      <Header/>
       <div>POKEDEX DAS MINAS</div>
       <Link to="/pokedex"><button>Ir Para Pokedex</button></Link>
     </>
