6ad4c71aefd7a042a14d9532ad9934a42b9551f7
Kell Lanes <kell@pop-os.localdomain>
2021-04-23 10:44:53 -0300

    erros console

--- pokedex/src/pages/PokedexPage.js
+++ pokedex/src/pages/PokedexPage.js
@@ -1,12 +1,10 @@
 import React, { useContext } from "react";
-import { useHistory, useParams } from "react-router-dom";
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import { useHistory} from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 import GlobalStateContext from "../GlobalState/GlobalContext";
 import { PokeListContainer } from "./HomePage";
 import PokemonCard from "../components/pokemonCard/pokemonCard";
-import { goToDetailsPage } from "../routes/coordinator"
-
 
 const PokedexPage = () => {
   const { pokedex } = useContext(GlobalStateContext);
@@ -17,13 +15,13 @@ const PokedexPage = () => {
       <Header />
       <PokeListContainer>
         {pokedex.map((poke) => {
-          return <PokemonCard key={poke.name} GoPokedex pokemon={poke} />
+          return <PokemonCard key={poke.name} GoPokedex pokemon={poke} />;
           // button deveria estar dentro do mapa pra pegar o poke.name
         })}
       </PokeListContainer>
       <Footer />
     </>
-  )
+  );
 };
 
 export default PokedexPage;
